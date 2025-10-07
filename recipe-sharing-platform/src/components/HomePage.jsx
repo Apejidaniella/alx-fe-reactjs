6bb84c964459ebe5291dc33123a513a3920509ac
Apejidaniella <apejidaniella@gmail.com>
2025-10-07 22:11:24 +0100

    added grid

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ function  HomePage() {
   }, [data]);
   return (
     <div className='p-4 flex-wrap '>
-        <div className='flex items-center gap-4 rounded-md shadow-xl hover:bg-slate-200 text-wrap'>
+        <div className='flex items-center gap-4 rounded-md shadow-xl hover:bg-slate-400 text-wrap grid grid-cols-1  grid-cols-2 grid-cols-3 sm '>
             {recipes.length > 0 && recipes.map((recipe, index) => (
             <RecipeCard key={index} recipe={recipe} />
         ))}
