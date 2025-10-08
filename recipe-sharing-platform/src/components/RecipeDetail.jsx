d919e8e174bc7aaf2011d4ef1a9de66dbc4275a2
Apejidaniella <apejidaniella@gmail.com>
2025-10-08 22:59:48 +0100

    created recipe detail component

---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import data from '../data.json';
+
+function RecipeDetail() {
+    const { id } = useParams(); 
+   const [recipe, setRecipe] = useState(null); 
+
+    useEffect(() => {
+         const foundRecipe = data.find(r => r.id.toString() === id);
+        setRecipe(foundRecipe); 
+    }, [id]);  
+
+  if (!recipe) {
+        return <div className='p-4 text-center'>Recipe not found.</div>;
+    }
+
+
+  return (
+    <div>
+        <img src={recipe.image} alt={recipe.title} className='rounded-xl h-[180px] w-full' />
+           <h3 className='mb-2 text-lg font-bold text-slate-900'>{recipe.title}</h3>
+           <p className='font-normal text-gray-700'>{recipe.summary} </p>
+    </div>
+  )
+}
+
+export default RecipeDetail;
