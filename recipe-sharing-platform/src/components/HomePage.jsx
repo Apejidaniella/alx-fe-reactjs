e5bf46e16fc1df48d26fbabef4a55d5b8e808ea4
Apejidaniella <apejidaniella@gmail.com>
2025-10-09 23:17:10 +0100

    added links to homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import data from '../data.json';
 import RecipeCard from './RecipeCard';
+import { Link } from 'react-router-dom';
 
 function  HomePage() {
   const [recipes, setRecipes] = useState([]);
@@ -12,7 +13,7 @@ function  HomePage() {
     <div className='p-4 flex-wrap '>
         <div className='flex items-center gap-4 rounded-md shadow-xl hover:bg-slate-400 text-wrap grid grid-cols-1  grid-cols-2 grid-cols-3 sm '>
             {recipes.length > 0 && recipes.map((recipe, index) => (
-            <RecipeCard key={index} recipe={recipe} />
+            <Link key={index} to={`/recipe/${recipe.id}`}> <RecipeCard  recipe={recipe} /></Link>
         ))}
 
         </div>
