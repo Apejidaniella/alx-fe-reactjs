c010401707aa2b05147696cdd4b101ac0f433569
Apejidaniella <apejidaniella@gmail.com>
2025-10-14 21:43:44 +0100

    renamed some properties

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useQuery } from 'react-query'
 
 function PostsComponent() {
-   const { isPending, error, data } = useQuery({
+   const { isPending, isLoading, isError, data:fetchPosts } = useQuery({
     queryKey: ['repoData'],
     queryFn: () =>
       fetch('https://jsonplaceholder.typicode.com/posts').then((res) =>
@@ -10,13 +10,13 @@ function PostsComponent() {
       ),
   })
     console.log(data)
-  if (isPending) return 'Loading...'
+  if (isLoading) return 'Loading...'
 
-  if (error) return 'An error has occurred: ' + error.message
+  if (isError) return 'An error has occurred: ' + error.message
 
   return (
     <div>
-      {data.map(post => (<div key={post.id}>
+      {fetchPosts.map(post => (<div key={post.id}>
         <h1>{post.name}</h1>
         <p>{post.description}</p>
       </div>))}
